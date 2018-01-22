dc6c44e361029a643ab19f5debdd6a046cd9da07
Christopher Loonam <chrisloonam@gmail.com>
2018-01-21 23:19:17 -0500

    Tree contruction successful

--- huffman/src/huffman.c
+++ huffman/src/huffman.c
@@ -10,41 +10,68 @@ typedef struct _Head {
 
 typedef struct _HuffNode {
 	char val;
+	int ascii;
+	struct _HuffNode *next;
 	struct _HuffNode *left;
 	struct _HuffNode *right;
 } HuffNode;
 
-char *_reverse(const char *str, int len) {
-	char *new = malloc(sizeof(*new)*len);
-	for (int i = 2; i <= len; i++) {
-		new[i-2] = str[len-i];
-	}
-	new[len-1] = '\0';
+HuffNode *_huff_new(char val) {
+	HuffNode *new = calloc(1, sizeof(*new));
+	new->val = val;
+	new->ascii = 1;
 	return new;
 }
 
+HuffNode *_huff_pop(HuffNode *list) {
+	while (list->next->next) {
+		list = list->next;
+	}
+	HuffNode *pop = list->next;
+	list->next = NULL;
+	return pop;
+}
+
+void _huff_push(HuffNode *list, HuffNode *new) {
+	while (list->next) {
+		list = list->next;
+	}
+	list->next = new;
+}
+
+void _huff_free(HuffNode *node) {
+	if (node->next) {
+		_huff_free(node->next);
+	}
+	if (node->left) {
+		_huff_free(node->left);
+	}
+	if (node->right) {
+		_huff_free(node->right);
+	}
+	free(node);
+}
+
 HuffNode *_build_tree(char *str) {
-	HuffNode *node = calloc(1, sizeof(*node));
+	HuffNode *root = _huff_new(0);
 	while (*str) {
-		putchar(*str);
-		if (*str == '0') {
-			if (!node->right) {
-				node->right = _build_tree(++str);
-			} else {
-				return node;
-			}
-		} else if (*str != '1') {
-			if (!node->right) {
-				node->right = calloc(1, sizeof(*node->right));
-				node->right->val = *str;
-			} else {
-				node->left = calloc(1, sizeof(*node->left));
-				node->left->val = *str;
-			}
+		if (*str == '1') {
+			HuffNode *new = _huff_new(*(++str));
+			_huff_push(root, new);
+		} else if (*str == '0') {
+			HuffNode *right = _huff_pop(root);
+			HuffNode *left = _huff_pop(root);
+			HuffNode *new = _huff_new(0);
+			new->ascii = 0;
+			new->left = left;
+			new->right = right;
+			_huff_push(root, new);
 		}
 		str++;
 	}
-	return node;	
+	HuffNode *ret = _huff_pop(root);
+	_huff_free(root);
+	return ret;
 }
 
 void _print_huff(HuffNode *node) {
@@ -66,19 +93,19 @@ int dehuff_c(const char *compressed_path, const char *code_path, const char *dec
 	if (!compressed_f) {
 		return EXIT_FAILURE;
 	}
+
 	Header header = {0};
 	fread(&header, sizeof(header), 1, compressed_f);
 	char *huff_raw = malloc(header.header_size);
 	fread(huff_raw, 1, header.header_size, compressed_f);	
-	char *reversed_huff = _reverse(huff_raw, header.header_size-1);
-	puts(reversed_huff);
-	HuffNode *root = _build_tree(reversed_huff);
-	putchar('\n');
-	_print_huff(root);
-
-	free(reversed_huff);
-	free(huff_raw);
+	huff_raw[header.header_size-1] = 0; //cuts off last 0
 	fclose(compressed_f);
+
+	HuffNode *root = _build_tree(huff_raw);
+	_print_huff(root);
+	free(huff_raw);
+
+	_huff_free(root);
 	return EXIT_SUCCESS;
 }
 
